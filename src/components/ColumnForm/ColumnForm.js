78f176e4cf59ffacf8034b8d4af9d0ff2f2829a9
Mateusz Kretowski <matkretkurs@gmail.com>
2023-02-11 20:57:43 +0100

    Add redux dispatch function to CardForm, ColumnForm and store

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,12 +2,14 @@ import styles from './ColumnForm.module.scss';
 import Button from '../Button/Button.js';
 import TextInput from '../TextInput/TextInput';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 const ColumnForm = (props) => {
+  const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.action({ title: title, icon: icon });
+    dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
     setTitle('');
     setIcon('');
   };
