015a304f04e5230024999798c0c61ee07f411891
Mateusz Kretowski <matkretkurs@gmail.com>
2023-02-15 23:55:39 +0100

    Add remove card function

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,31 @@
 import styles from './Card.module.scss';
 import { useDispatch } from 'react-redux';
-import { addToFavorites } from '../../redux/cardsRedux';
+import { addToFavorites, removeCard } from '../../redux/cardsRedux';
 import clsx from 'clsx';
 
 const Card = (props) => {
   const dispatch = useDispatch();
   const cardId = props.id;
-  const handleSubmit = (e) => {
+  const handleAddToFav = (e) => {
     e.preventDefault();
     dispatch(addToFavorites(cardId));
+  };
+  const handleRemove = (e) => {
+    e.preventDefault();
+    dispatch(removeCard(cardId));
   };
 
   return (
     <li className={styles.card}>
       {props.title}
-      <button onClick={handleSubmit} className={clsx(styles.button, props.isFavorite && styles.active)}>
-        <span className='fa fa-star-o' />
-      </button>
+      <div>
+        <button onClick={handleAddToFav} className={clsx(styles.button, props.isFavorite && styles.active)}>
+          <span className='fa fa-star-o' />
+        </button>
+        <button onClick={handleRemove} className={styles.button}>
+          <span className='fa fa-trash' />
+        </button>
+      </div>
     </li>
   );
 };
